b32c0d8ebc9ed799a329559be481fef4f51545c1
santhoshi.g <santhoshi.gondesi15@gmail.com>
2023-06-26 17:17:59 +0530

    added a sql file

--- dbscripts/test.sql
+++ dbscripts/test.sql
@@ -1,3 +1,5 @@
 create or replace schema demo_schema;
 use schema demo_schema;
 Create or replace table customer_Dim (id integer, name varchar);
+create or replace table customer_dim2(id integer,name varchar);
+create schema new_schema;
