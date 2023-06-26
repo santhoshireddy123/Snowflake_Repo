273053c76d6df73bc1732f13cc8d196b2f527f1c
santhoshi.g <santhoshi.gondesi15@gmail.com>
2023-06-26 17:13:51 +0530

    modified python fiule

--- snowflake_pipeline.py
+++ snowflake_pipeline.py
@@ -11,30 +11,40 @@ conn = snowflake.connector.connect(
         schema=os.environ["SNOWFLAKE_SCHEMA"],
     )
 
-
 warehouse = os.environ["SNOWFLAKE_WAREHOUSE"]
 database = os.environ["SNOWFLAKE_DATABASE"]
 schema = os.environ["SNOWFLAKE_SCHEMA"]
 
-def run_query(connection, query):
+def run_query(connection,sql):
     cursor = connection.cursor()
-    cursor.execute(query)
+    cursor.execute(sql)
     cursor.close()
 
-def snowflake_context(warehouse, database, schema):
-    warehouse_sql = 'use warehouse {}'.format(warehouse)
-    run_query(conn, warehouse_sql)
+warehouse_sql = 'use warehouse {}'.format(warehouse)
+run_query(conn, warehouse_sql)
 
-    sql = 'use database {}'.format(database)
-    run_query(conn, sql)
-
-    sql = 'use schema {}'.format(schema)
-    run_query(conn, sql)
-
-snowflake_context(warehouse,database,schema)
-
-
-sql = 'create or replace table customer_cicd_pipeline(id integer,name varchar);'
+sql = 'use database {}'.format(database)
 run_query(conn, sql)
+
+sql = 'use schema {}'.format(schema)
+run_query(conn, sql)
+
+
+sql_folder = './sql_scripts'
+
+with conn.cursor() as cursor:
+    for file_name in os.listdir(sql_folder):
+        if file_name.endswith('.sql'):
+            with open(os.path.join(sql_folder, file_name), 'r') as f:
+                sql_commands = f.read().split(';')
+                for sql in sql_commands:
+                    if sql.strip():
+                        cursor.execute(sql)
+
+
+
+
+
+
 
 
