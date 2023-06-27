aa670b80f3d1858a741abaccf09bf61588d82911
santhoshi.g <santhoshi.gondesi15@gmail.com>
2023-06-27 17:29:37 +0530

    mod .py file

--- pipeline_changed_files.py
+++ pipeline_changed_files.py
@@ -10,18 +10,21 @@ database = os.environ['SNOWFLAKE_DATABASE']
 schema = os.environ['SNOWFLAKE_SCHEMA']
 changed_sql_files = os.environ['changed_sql_files'].split()
 
+
 with connect(
-            account=account,
-            user=user,
-            password=password,
-            role=role,
-            warehouse=warehouse,
-            database=database,
-            schema=schema,
-        ) as connection:
+    account=account,
+    user=user,
+    password=password,
+    role=role,
+    warehouse=warehouse,
+    database=database,
+    schema=schema,
+   ) as connection:
     for sql_file in changed_sql_files:
         with open(sql_file, 'r') as f:
             sql = f.read().split(';')
             for sql_command in sql:
-                connection.cursor().execute(sql)
+                if sql_command.strip():
+                    connection.cursor().execute(sql_command)
+            print(f'Executing {sql_file}...')
             print(f'Successfully executed {sql_file}')
