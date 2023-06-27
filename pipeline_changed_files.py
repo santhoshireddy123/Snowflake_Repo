41dd5a88308157fb8809d862ff538f4e562f031d
santhoshi.g <santhoshi.gondesi15@gmail.com>
2023-06-27 23:40:32 +0530

    ,,,,,,,,,,,,

--- pipeline_changed_files.py
+++ pipeline_changed_files.py
@@ -23,10 +23,9 @@ with connect(
   
     for sql_file in changed_sql_files:
         with open(sql_file, 'r') as f:
-            print(f.read())
-        #     sql = f.read().split(';')
-        #     for sql_command in sql:
-        #         if sql_command.strip():
-        #             connection.cursor().execute(sql_command)
-        #     print(f'Executing {sql_file}...')
+            sql = f.read().split(';')
+            for sql_command in sql:
+                if sql_command.strip():
+                    connection.cursor().execute(sql_command)
+                    print(f'Executing {sql_file}...')
         #     print(f'Successfully executed {sql_file}')
